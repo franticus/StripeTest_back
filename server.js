5028344ff66735ec404e73ceb194f1765cba8b1e
S.Zelensky <zelenstein@gmail.com>
2024-06-04 13:19:20 +0300

    PORT 2

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static(path.join(__dirname, '../')));
 
 // Endpoint to retrieve the API key
 app.get('/get-api-key', (req, res) => {
+  console.log('Request received to get API key');
   res.json({ apiKey: process.env.MY_API_KEY });
 });
 
@@ -21,24 +22,30 @@ const validateApiKey = (req, res, next) => {
   const apiKey = req.headers.authorization;
 
   if (!apiKey) {
+    console.error('Authorization header is missing (server)');
     return res.status(401).send('Authorization header is missing (server)');
   }
 
   if (!apiKey.startsWith('Bearer ')) {
+    console.error('Invalid authorization format (server)');
     return res.status(401).send('Invalid authorization format (server)');
   }
 
   const apiKeyValue = apiKey.split(' ')[1];
 
   if (apiKeyValue !== process.env.MY_API_KEY) {
+    console.error('Invalid API key (server)');
     return res.status(403).send('Invalid API key (server)');
   }
 
+  console.log('API key validated successfully');
   next();
 };
 
 app.post('/create-checkout-session', validateApiKey, async (req, res) => {
   try {
+    console.log('Creating checkout session');
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: [
@@ -48,7 +55,7 @@ app.post('/create-checkout-session', validateApiKey, async (req, res) => {
             product_data: {
               name: 'Test Product',
             },
-            unit_amount: 500,
+            unit_amount: 5000, // Изменила на 5000 для большего значения
           },
           quantity: 1,
         },
@@ -58,13 +65,15 @@ app.post('/create-checkout-session', validateApiKey, async (req, res) => {
       cancel_url: 'https://stripetestiq.netlify.app/cancel.html',
     });
 
+    console.log('Checkout session created successfully');
     res.json({ id: session.id });
   } catch (error) {
+    console.error('Error creating checkout session:', error.message);
     res.status(500).json({ error: error.message });
   }
 });
 
-const PORT = process.env.PORT || 4242;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
