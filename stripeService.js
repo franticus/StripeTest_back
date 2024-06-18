d098f6f6fee0b3af6fdfb72b073aed4eec60d795
S.Zelensky <zelenstein@gmail.com>
2024-06-18 10:50:20 +0300

    refactor v1 del base

--- stripeService.js
+++ stripeService.js
@@ -1,104 +1,5 @@
 const stripeLive = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const stripeDev = require('stripe')(process.env.STRIPE_SECRET_KEY_DEV);
-const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');
-const path = require('path');
-
-let dbPath;
-
-if (
-  fs.existsSync('/data') &&
-  fs.existsSync(path.join('/data', 'database.sqlite'))
-) {
-  dbPath = path.resolve('/data', 'database.sqlite');
-} else {
-  dbPath = path.resolve(__dirname, 'data', 'database.sqlite');
-}
-
-const db = new sqlite3.Database(dbPath, err => {
-  if (err) {
-    console.error('Error opening database:', err.message);
-  } else {
-    console.log('Connected to the SQLite database.');
-  }
-});
-
-// Functions for interacting with the database
-const addUser = (
-  id,
-  date,
-  userName,
-  email,
-  payment_method_types,
-  amount_total,
-  amount_subtotal,
-  currency,
-  userId,
-  iqValue,
-  mode,
-  status,
-  subscription_id,
-  customer_id
-) => {
-  const stmt = db.prepare(
-    'INSERT INTO users (id, date, userName, email, payment_method_types, amount_total, amount_subtotal, currency, userId, iqValue, mode, status, subscription_id, customer_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-  );
-  stmt.run(
-    id,
-    date,
-    userName,
-    email,
-    JSON.stringify(payment_method_types),
-    amount_total,
-    amount_subtotal,
-    currency,
-    userId,
-    iqValue,
-    mode,
-    status,
-    subscription_id,
-    customer_id,
-    err => {
-      if (err) {
-        console.error('Error inserting user:', err.message);
-      } else {
-        console.log('User inserted successfully');
-      }
-    }
-  );
-  stmt.finalize(err => {
-    if (err) {
-      console.error('Error finalizing statement:', err.message);
-    }
-  });
-};
-
-const addBeforeCheckout = (userId, userName, email, date, iqValue) => {
-  const stmt = db.prepare(
-    'INSERT INTO beforeCheckout (userId, userName, email, date, iqValue) VALUES (?, ?, ?, ?, ?)'
-  );
-  stmt.run(userId, userName, email, date, iqValue, err => {
-    if (err) {
-      console.error('Error inserting user before checkout:', err.message);
-    } else {
-      console.log('Before checkout data inserted successfully');
-    }
-  });
-  stmt.finalize(err => {
-    if (err) {
-      console.error('Error finalizing statement:', err.message);
-    }
-  });
-};
-
-const getUserByEmail = (email, callback) => {
-  db.get('SELECT * FROM users WHERE email = ?', [email], (err, row) => {
-    if (err) {
-      console.error('Error retrieving user:', err.message);
-    }
-    callback(err, row);
-  });
-};
 
 // Stripe functions
 const createStripeCustomer = async (email, userName, stripe) => {
@@ -149,58 +50,43 @@ const createCheckoutSession = async (
     client_reference_id: subscription.id,
   });
 
-  const date = new Date().toISOString();
-  addUser(
-    session.id,
-    date,
-    userName,
-    email,
-    session.payment_method_types,
-    session.amount_total,
-    session.amount_subtotal,
-    session.currency,
-    userId,
-    iqValue,
-    session.mode,
-    session.status,
-    subscription.id,
-    customer.id
-  );
-
   return session;
 };
 
-const createBillingPortalSession = async (email, origin) => {
-  return new Promise((resolve, reject) => {
-    getUserByEmail(email, async (err, user) => {
-      if (err || !user) {
-        return reject('User not found');
-      }
-
-      const stripe = origin.includes('iq-check140.com')
-        ? stripeLive
-        : stripeDev;
-      const session = await stripe.billingPortal.sessions.create({
-        customer: user.customer_id,
-        return_url: `${origin}/#/home`,
-      });
-
-      resolve(session.url);
-    });
+const createBillingPortalSession = async (customerId, origin) => {
+  const stripe = origin.includes('iq-check140.com') ? stripeLive : stripeDev;
+  const session = await stripe.billingPortal.sessions.create({
+    customer: customerId,
+    return_url: `${origin}/#/home`,
   });
+
+  return session.url;
 };
 
-const checkSubscription = email => {
-  return new Promise((resolve, reject) => {
-    getUserByEmail(email, (err, user) => {
-      if (err || !user) {
-        return reject('User not found');
-      }
-
-      const hasSubscription = !!user.subscription_id;
-      resolve({ hasSubscription });
+const checkSubscription = async email => {
+  const stripe = email.includes('iq-check140.com') ? stripeLive : stripeDev;
+  try {
+    const customers = await stripe.customers.list({
+      email: email,
+      limit: 1,
     });
-  });
+
+    if (customers.data.length === 0) {
+      return { hasSubscription: false };
+    }
+
+    const customerId = customers.data[0].id;
+    const subscriptions = await stripe.subscriptions.list({
+      customer: customerId,
+      status: 'active',
+      limit: 1,
+    });
+
+    return { hasSubscription: subscriptions.data.length > 0 };
+  } catch (error) {
+    console.error('Error checking subscription status: ', error);
+    return { hasSubscription: false };
+  }
 };
 
 const handleWebhookEvent = async (event, origin) => {
@@ -209,23 +95,7 @@ const handleWebhookEvent = async (event, origin) => {
   switch (event.type) {
     case 'checkout.session.completed':
       const session = event.data.object;
-      getUserByEmail(session.customer_email, (err, user) => {
-        if (user) {
-          db.run(
-            'UPDATE users SET subscription_id = ?, payment_status = ? WHERE email = ?',
-            [session.client_reference_id, 'completed', session.customer_email],
-            err => {
-              if (err) {
-                console.error('Error updating user:', err.message);
-              } else {
-                console.log('User updated successfully');
-              }
-            }
-          );
-        } else {
-          console.error('User not found');
-        }
-      });
+      // Обработка события
       break;
     default:
       console.log(`Unhandled event type ${event.type}`);
@@ -237,5 +107,4 @@ module.exports = {
   createBillingPortalSession,
   checkSubscription,
   handleWebhookEvent,
-  addBeforeCheckout,
 };
